861d574fbc99276b7c2e5060dc61a1dce8e62ea3
João Cunha <skidrow.jc@gmail.com>
2021-10-09 16:50:40 +0100

    Better scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
+mp="modprobe.d/"
+md="modules-load.d/"
+
 install -dm 555 "/etc/X11/xorg.conf.d"
-install -Dm 644 10-nvidia.conf "/etc/X11/nvidia-xorg.conf"
+install -Dm 644 nvidia-xorg.conf "/etc/X11/nvidia-xorg.conf"
 install -Dm 644 nvidia-xinitrc "$HOME/.nvidia-xinitrc"
 install -Dm 755 nvidia-xrun "/usr/bin/nvidia-xrun"
+
+for i in ${mp}*; do
+  install -Dm 644 "${i}"  "/etc/${mp}"
+done
+
+for i in ${md}*; do
+  install -Dm 644 "${i}"  "/etc/${md}"
+done
